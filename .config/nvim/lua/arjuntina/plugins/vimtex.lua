72af1c6e32700c295d211f57b02283c9b370f4eb
Arjun Patel <nujra911@gmail.com>
2023-12-15 16:00:59 -0500

    hi

--- .config/nvim/lua/arjuntina/plugins/vimtex.lua
+++ .config/nvim/lua/arjuntina/plugins/vimtex.lua
@@ -7,6 +7,9 @@ return {
         -- set the default pdf viewer
         vim.cmd([[let g:vimtex_view_method = 'zathura']])
         -- explicitly set the local leader (same as default key)
-        -- vim.cmd([[let maplocalleader = "\"]])
+        vim.cmd([[let maplocalleader = "\\"]])
+        -- conceal latex delimiter code unless you are editing the specific line of the text!
+        -- eg. replaces $ delimiters & \in + \cap by their corresponding unicode symbols
+        vim.cmd([[set conceallevel=1]])
     end,
 }
--- .config/nvim/lua/arjuntina/remap.lua
+++ .config/nvim/lua/arjuntina/remap.lua
@@ -15,16 +15,35 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", {desc = "Move window focus left"})
 vim.keymap.set("n", "<C-j>", "<C-w>j", {desc = "Move window focus down"})
 vim.keymap.set("n", "<C-k>", "<C-w>k", {desc = "Move window focus up"})
 vim.keymap.set("n", "<C-l>", "<C-w>l", {desc = "Move window focus right"})
-vim.keymap.set("n", "<leader>wh", "<C-w>h")
+--vim.keymap.set("n", "<leader>wh", "<C-w>h")
 vim.keymap.set("n", "<leader>wj", "<C-w>j")
 vim.keymap.set("n", "<leader>wk", "<C-w>k")
-vim.keymap.set("n", "<leader>wl", "<C-w>l")
+--vim.keymap.set("n", "<leader>wl", "<C-w>l")
+
+
 -- resizing windows
 -- spc-w-Direction
-vim.keymap.set("n", "<leader>wH", "<C-w><")
-vim.keymap.set("n", "<leader>wL", "<C-w>>")
 vim.keymap.set("n", "<leader>wK", "") -- figure out!
 vim.keymap.set("n", "<leader>wJ", "") -- figure out!
+
+
+vim.keymap.set('n', '<leader>wh', '<Cmd>BufferPrevious<CR>')
+vim.keymap.set('n', '<leader>wl', '<Cmd>BufferNext<CR>')
+vim.keymap.set('n', '<leader>wH', "<Cmd>BufferMovePrevious<CR>'")
+vim.keymap.set('n', '<leader>wL', "<Cmd>BufferMoveNext<CR>'")
+
+vim.keymap.set('n', '<leader>w1', '<Cmd>BufferGoto 1<CR>')
+vim.keymap.set('n', '<leader>w2', '<Cmd>BufferGoto 2<CR>')
+vim.keymap.set('n', '<leader>w3', '<Cmd>BufferGoto 3<CR>')
+vim.keymap.set('n', '<leader>w4', '<Cmd>BufferGoto 4<CR>')
+vim.keymap.set('n', '<leader>w5', '<Cmd>BufferGoto 5<CR>')
+vim.keymap.set('n', '<leader>w6', '<Cmd>BufferGoto 6<CR>')
+vim.keymap.set('n', '<leader>w7', '<Cmd>BufferGoto 7<CR>')
+vim.keymap.set('n', '<leader>w8', '<Cmd>BufferGoto 8<CR>')
+vim.keymap.set('n', '<leader>w9', '<Cmd>BufferGoto 9<CR>')
+vim.keymap.set('n', '<leader>w0', '<Cmd>BufferLast<CR>')
+
+vim.keymap.set('n', '<leader>wc', '<Cmd>BufferClose<CR>')
 
 -- Tabs!
 vim.keymap.set("n", "<C-t>", "<cmd>tabnew<CR>", {desc = "open new tab"})
